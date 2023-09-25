a0861edb014dc6145fcc1765782b76ff285ef61b
Sruthi <sruthimangala2000@gmail.com>
2023-09-25 20:18:48 +0200

    Added basic code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Simple_Calculator
+
+
+1. Implement simple calculator function in terminal
+2. Add error handling
+3. Add GUI
+4. Add documentation
--- main.cpp
+++ main.cpp
@@ -2,16 +2,51 @@
 
 using namespace std;
 
+float calculate(int num1, int num2, char operatorSymbol) {
+    float result = 0;
+    switch (operatorSymbol)
+    {
+    case '+':
+        result = num1 + num2 ;
+        break;
+
+    case '-':
+        result = num1 - num2 ;
+        break;
+
+    case '*':
+        result = num1 * num2 ;
+        break;
+
+    case '/':
+        result = num1 / num2 ;
+        break;
+    
+    default:
+        result = 0;
+        break;
+    }
+
+    return result;
+}
+
 int main(){
-    int a;
-    int b;
-    int c;
-    cout << "enter two numbers" << endl;
-    cin >> a >> b;
-    cout << a << b << endl;
-    c=a;
-    a=b;
-    b=c;
-    cout << "The reversed numbers are " << endl << a << b <<endl;
+    char operation;
+    int num1;
+    int num2;
+    float finalResult = 0;
+
+    cout << "Enter the operation you wish to perform." << endl 
+         << "Options include addition (+) subtraction (-) multiplication (*) and division (/)" << endl;
+    cin >> operation;
+
+    cout << "Enter the two numbers you wish to operate : " << endl;
+    cin >> num1 >> num2 ;
+
+    finalResult = calculate(num1, num2, operation);
+
+    cout << num1 << " " << operation << " " << num2 << " = " << finalResult ;
+
+
     return 0;
 }
